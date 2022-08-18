ae612821eaa190c9bb95ba5a1b5f7a1f80d49c39
TC Teo <tsechin@tsechin.com>
2022-08-18 22:12:19 +1000

    improve ve.sh and run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,19 @@
-_ve/bin/python -m "$@"
+#!/bin/bash
+set -e
+__DIR="$( cd "$( readlink -f $(dirname "${BASH_SOURCE[0]}" ))" && pwd )"
+ve_dir="${__DIR}/_ve"
+
+if [[ ! -d "${ve_dir}" ]]; then
+  echo "virtualenv dir missing, run ve.sh"
+fi
+
+module="$1"
+shift || true
+
+# remove trailing slash
+module="$(echo "${module}" | sed -e 's/\/$//g')"
+# replace slashes with dots
+module="$(echo "${module}" | sed -e 's/\//./g')"
+
+set -x
+"${ve_dir}/bin/python" -m "${module}" "$@"
--- ve.sh
+++ ve.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 __DIR="$( cd "$( readlink -f $(dirname "${BASH_SOURCE[0]}" ))" && pwd )"
-
-if [ -z "$1" ]; then
-  ve_dir="${__DIR}/_ve"
-else
-  ve_dir="${1}"
-fi
+ve_dir="${__DIR}/_ve"
 
 if [ ! -d "${ve_dir}" ]; then
   virtualenv -p "$(which python3)" "${ve_dir}"
