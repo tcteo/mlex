6b306e597092113219f47bfad431be25841e3658
TC Teo <tsechin@tsechin.com>
2022-08-19 08:19:14 +1000

    auto formatting

--- openai_completion/main.py
+++ openai_completion/main.py
@@ -6,9 +6,12 @@ import sys
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--max_tokens', type=int, default=200, help='max_tokens (openai.Completion parameter)')
-  parser.add_argument('--temperature', type=float, default=0.3, help='temperature (openai.Completion parameter)')
-  parser.add_argument('--engine', type=str, default='text-curie-001', help='engine (openai.Completion parameter)')
+  parser.add_argument('--max_tokens', type=int, default=200,
+                      help='max_tokens (openai.Completion parameter)')
+  parser.add_argument('--temperature', type=float, default=0.3,
+                      help='temperature (openai.Completion parameter)')
+  parser.add_argument('--engine', type=str, default='text-curie-001',
+                      help='engine (openai.Completion parameter)')
   args = parser.parse_args()
 
   apikey = os.environ.get('OPENAI_API_KEY', None)
@@ -23,10 +26,10 @@ def main():
   input = ''.join(lines)
 
   response = openai.Completion.create(
-    engine=args.engine,
-    prompt=input,
-    temperature=args.temperature,
-    max_tokens=args.max_tokens)
+      engine=args.engine,
+      prompt=input,
+      temperature=args.temperature,
+      max_tokens=args.max_tokens)
 
   print('\nResponse:')
 
